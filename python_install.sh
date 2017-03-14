6bbfcdeed97c02f505e8acd69af8809470993a48
Ben Coomes <bcoomes@g.clemson.edu>
2017-03-14 10:28:29 -0400

    changed to silent anaconda install script

--- python_install.sh
+++ python_install.sh
@@ -4,6 +4,13 @@ source ~/.bash_profile
 
 wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
 
-bash Anaconda3-4.3.1-Linux-x86_64.sh
+bash Anaconda3-4.3.1-Linux-x86_64.sh -b -p $HOME/anaconda3
+
+NEWPATH = 'PATH=$PATH:/home/$USER/anaconda3/bin'
+
+echo $NEWPATH >> ~/.bash_profile
+
+source ~/.bash_profile
 
 pip install --user mpi4py #should we install as user? 
+
