49b5639b628f726679b7af52807be2e906ce493c
Ben Coomes <bcoomes@g.clemson.edu>
2017-03-16 16:56:05 -0400

    trying Ngo's mpi install

--- mpi_install.sh
+++ mpi_install.sh
@@ -6,18 +6,23 @@ tar -xvf openmpi-1.10.3.tar.gz
 
 cd openmpi-1.10.3
 
-./configure --prefix="/users/$USER/.openmpi"
+./configure --prefix="/software/openmpi/1.10.3"
 
+# below should be run as root
+#############################
+make 
 make install
 
 NEWPATH='PATH=$PATH:/home/$USER/.openmpi/bin'
 NEWLD_LIB='LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/users/$USER/.openmpi/lib/'
 EXPORT_LDLIB='export LD_LIBRARY_PATH'
 
-echo $NEWPATH >> ~/.bash_profile
-echo $NEWLD_LIB >> ~/.bash_profile
-echo $EXPORT_LDLIB >> ~/.bash_profile
+echo $NEWPATH >> /etc/environment
+echo $NEWLD_LIB >> /etc/environment
+source /etc/environment
+############################
+#above should be run as root
 
-source ~/.bash_profile
-echo "BASH PROFILE:"
-cat ~/.bash_profile
+echo "INSTALL VERIFICATION"
+printenv PATH
+which mpicc
