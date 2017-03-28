3dd451f824feddddec0c01eb75f1951c72fd9446
mdrodgers <mdrodge@g.clemson.edu>
2017-03-28 19:33:53 -0400

    Create backup_lclientone.sh

---
+++ backup_lclientone.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# from http://lustre.ornl.gov/lustre101-courses/content/C1/L3/LustreBasicInstall.pdf
+
+# Prepare servers and client
+   # First install minimal CentOS
+   # Disable SELinux
+   # Set SELINUX=disabled in /etc/sysconfig/selinux
+   echo "----- Disable SELinux -----"
+   if [ ! -e /etc/sysconfig/selinux ]; then
+      echo "SELINUX=disabled" > /etc/sysconfig/selinux
+   else
+      exists='cat /etc/sysconfig/selinux | grep SELINUX'
+      if [ exists ]; then
+         sed -ie 's/SELINUX=.*$/SELINUX=disabled/' /etc/sysconfig/selinux
+      else
+         echo "SELINUX=disabled" >> /etc/sysconfig/selinux
+      fi
+   fi
+
+   # Disable iptables
+   echo "----- Disable iptables -----"
+   chkconfig --levels 345 iptables off
+   chkconfig --levels 345 ip6tables off
+   #reboot # Gotta make sure the script keeps going
