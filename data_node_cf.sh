8948492740965574f09ad250035bd58ecd6f5e49
Benjamin Coomes <bcoomes@g.clemson.edu>
2017-03-29 15:04:02 -0400

    work on envmodules install

--- data_node_cf.sh
+++ data_node_cf.sh
@@ -8,6 +8,9 @@ echo "Starting mpi install, this may take a few minutes..."
 ssh -t $1 'sudo /teamKMBR/mpi_install.sh'
 echo "Done.\n"
 
+ssh -t $1 'sudo yum -y environment-modules'
+ssh -t $1 'sudo cp /teamKMBR/openmpi-1.10.3 /usr/share/Modules/modulefiles/openmpi-1.10.3' 
+
 echo "Starting python install, this may take a few minutes..."
 ssh $1  '/teamKMBR/python_install.sh'
 echo "Done. Node successfully configured!" 
