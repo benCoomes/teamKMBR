acffc6e98adc353bc493967057021ecf0ed10c02
Matthew Rodgers (macbook) <mattmusic1117@gmail.com>
2017-03-30 09:23:59 -0400

    partitioning scripts again

---
+++ config_mgs_mgt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# MGS/MDS Configuration / Starting the File System ( [root@mgs_mds]$ )
+   # Format the MGT
+   mkfs.lustre --mgs /dev/sdb
+
+   # Format the MDT
+   mkfs.lustre --fsname=lustre \
+   --mgsnode=mgs_mds@tcp --mdt --index=0 /dev/sdc
+   
+   # Mount the MGT
+   mkdir /mnt/mgt
+   mount –t lustre /dev/sdb /mnt/mgt
+   
+   # Mount the MDT
+   mkdir /mnt/mdt
+   mount –t lustre /dev/sdc /mnt/mdt
