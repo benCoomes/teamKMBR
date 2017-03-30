00f96e8033ec349e91e7a3467a5f5e0009027615
Ben Coomes <bcoomes@g.clemson.edu>
2017-03-30 19:02:28 -0400

    added python mpi test

---
+++ mpitest.py
@@ -0,0 +1,9 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+name = MPI.Get_processor_name()
+print("Hello from process" + rank 
+   + "running on host" + name 
+   + "out of" + size + "processes")
