0cc3c4983e12cb98f85fe97b6734675970460436
mdrodgers <mdrodge@g.clemson.edu>
2017-03-30 09:56:06 -0400

    Update setup_server_pre.sh

--- setup_server_pre.sh
+++ setup_server_pre.sh
@@ -2,7 +2,7 @@
 
 # Install Lustre on Servers
    # Download Lustre server kernel
-   wget https://downloads.hpdd.intel.com/public/lustre/lustre-2.7.0/el6.6/server/RPMS/x86_64/kernel-2.6.32-504.8.1.el6_lustre.x86_64.rpm
+   wget https://downloads.hpdd.intel.com/public/lustre/lustre-2.7.0/el6.6/server/RPMS/x86_64/kernel-2.6.32-504.8.1.el6_lustre.x86_64.rpm --no-check-certificate
 
    # Install the kernel RPM
    rpm -ivh kernel-2.6.32-504.8.1.el6_lustre.x86_64.rpm
