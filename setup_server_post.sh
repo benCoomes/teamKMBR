808c291621923bd95c2dfeedf998afbda9dc7143
Matthew Rodgers (macbook) <mattmusic1117@gmail.com>
2017-04-03 10:50:47 -0400

    setup working now

--- setup_server_post.sh
+++ setup_server_post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-   yum install net-snmp net-snmp-utils libyaml sg3_utils -y
+   yum openmpi install net-snmp net-snmp-utils libyaml sg3_utils -y
 
    # Download Lustre server modules
    # from https://downloads.hpdd.intel.com/public/lustre/lustre-2.7.0/el6.6/server/RPMS/x86_64/
