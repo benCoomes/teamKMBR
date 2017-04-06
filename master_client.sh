5343c43c9af917d5605e51b5c3548032ebd91e0a
mdrodgers <mdrodge@g.clemson.edu>
2017-04-06 16:29:35 -0400

    Update master_client.sh

--- master_client.sh
+++ master_client.sh
@@ -7,13 +7,13 @@ if [ -e "$workingpath"config_client_log.txt ]; then
    echo "nothing" > /dev/null
 elif [ -e "$workingpath"client_log.txt ]; then
    # server post and config mgs_mdt or oss
-   sudo "$workingpath"setup_client_post.sh | tee -a "$workingpath"client_log.txt
-   sudo "$workingpath"config_client.sh | tee -a "$workingpath"config_client_log.txt
+   sudo "$workingpath"setup_client_post.sh 2>&1 | tee -a "$workingpath"client_log.txt
+   sudo "$workingpath"config_client.sh 2>&1 | tee -a "$workingpath"config_client_log.txt
 elif [ -e "$workingpath"setup_log.txt ]; then
    # setup post and server pre
-   sudo "$workingpath"setup_post.sh | tee -a "$workingpath"setup_log.txt
-   sudo "$workingpath"setup_client_pre.sh | tee -a "$workingpath"client_log.txt
+   sudo "$workingpath"setup_post.sh 2>&1 | tee -a "$workingpath"setup_log.txt
+   sudo "$workingpath"setup_client_pre.sh 2>&1 | tee -a "$workingpath"client_log.txt
 else
    # setup pre
-   sudo "$workingpath"setup_pre.sh | tee -a "$workingpath"setup_log.txt
+   sudo "$workingpath"setup_pre.sh 2>&1 | tee -a "$workingpath"setup_log.txt
 fi
