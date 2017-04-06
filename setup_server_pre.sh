a4934ab4f30cf160257abf3e2b2978dfc1b16690
mdrodgers <mdrodge@g.clemson.edu>
2017-04-06 17:01:17 -0400

    Update setup_server_pre.sh

--- setup_server_pre.sh
+++ setup_server_pre.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+   cd /root/teamKMBR
+
 # Install Lustre on Servers
    # Download Lustre server kernel
    wget https://downloads.hpdd.intel.com/public/lustre/lustre-2.7.0/el6.6/server/RPMS/x86_64/kernel-2.6.32-504.8.1.el6_lustre.x86_64.rpm --no-check-certificate
