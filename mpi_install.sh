4230a183b0ff4b21378cf881b09f0f1e07d5d745
Ben Coomes <bcoomes@g.clemson.edu>
2017-04-06 19:23:55 -0400

    asdfasdfa

--- mpi_install.sh
+++ mpi_install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#wget https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.3.tar.gz
+wget https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.3.tar.gz
 #use git repo, above link not stable
-cp /teamKMBR/openmpi-1.10.3.tar.gz ~/
-tar -xvf ~/openmpi-1.10.3.tar.gz 
-rm ~/openmpi-1.10.3.tar.gz
-cd ~/openmpi-1.10.3
+tar -xvf openmpi-1.10.3.tar.gz 
+rm openmpi-1.10.3.tar.gz
+cd openmpi-1.10.3
 
 ./configure --prefix="/software/openmpi/1.10.3"
 make 
 make install
 
-# is it okay to remove this? 
-rm -rf ~/openmpi-1.10.3
-rmdir ~/openmpi-1.10.3
+cd ..
+rm -rf openmpi-1.10.3
 #don't need these - path setting is handled by environment-modules
 #echo export PATH="$PATH:/software/openmpi/1.10.3/bin" >> /$HOME/.bashrc
 #echo export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/software/openmpi/1.10.3/lib/" >>/etc/environment
