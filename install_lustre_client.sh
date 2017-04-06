dec989f11807070fb773e5a661c54c93a9f7eb94
mdrodgers <mdrodge@g.clemson.edu>
2017-04-06 15:01:34 -0400

    Update install_lustre_client.sh

--- install_lustre_client.sh
+++ install_lustre_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo echo "/users/mdrodge/teamKMBR/master_client.sh" >> /etc/rc.local
+sudo echo "/root/teamKMBR/master_client.sh" >> /etc/rc.local
 sudo chmod +x /etc/rc.local
 sudo sed -i '/requiretty/d' /etc/sudoers
-sudo /users/mdrodge/teamKMBR/master_client.sh
+sudo /root/teamKMBR/master_client.sh
