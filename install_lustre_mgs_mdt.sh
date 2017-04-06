09f2e9d871a8169f4f3d5aae983267abc78cc0dc
mdrodgers <mdrodge@g.clemson.edu>
2017-04-06 15:00:39 -0400

    Update install_lustre_mgs_mdt.sh

--- install_lustre_mgs_mdt.sh
+++ install_lustre_mgs_mdt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo echo "/users/mdrodge/teamKMBR/master_mgs_mdt.sh" >> /etc/rc.local
+sudo echo "/root/teamKMBR/master_mgs_mdt.sh" >> /etc/rc.local
 sudo chmod +x /etc/rc.local
 sudo sed -i '/requiretty/d' /etc/sudoers
-sudo /users/mdrodge/teamKMBR/master_mgs_mdt.sh
+sudo /root/teamKMBR/master_mgs_mdt.sh
