91fdbc39d20f0952d5905ef371a160032e2651af
Matthew Rodgers (macbook) <mattmusic1117@gmail.com>
2017-04-06 19:13:02 -0400

    ips to names

--- config_oss.sh
+++ config_oss.sh
@@ -3,8 +3,10 @@
 # OSS Configuration ( [root@oss]$ )
    # Format the OSTs on the OSS
    mkfs.lustre --fsname=lustre --ost \
-   --mgsnode=128.104.222.29@tcp --index=0 /dev/sdb
+   --mgsnode=mgs_mdt@tcp --index=0 /dev/sdb
+   #--mgsnode=128.104.222.29@tcp --index=0 /dev/sdb
 
    # Mount the OSTs
    mkdir /mnt/ost0
-   mount –t lustre /dev/sdb /mnt/ost0
+   #mount –t lustre /dev/sdb /mnt/ost0
+   sudo mount -t lustre /dev/sdb /mnt/ost0
