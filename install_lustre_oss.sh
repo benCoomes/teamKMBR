1f21ae7d8c060744cdffa86dd3943716a5202cbc
mdrodgers <mdrodge@g.clemson.edu>
2017-04-06 15:02:12 -0400

    Update install_lustre_oss.sh

--- install_lustre_oss.sh
+++ install_lustre_oss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo echo "/users/mdrodge/teamKMBR/master_oss.sh" >> /etc/rc.local
+sudo echo "/root/teamKMBR/master_oss.sh" >> /etc/rc.local
 sudo chmod +x /etc/rc.local
 sudo sed -i '/requiretty/d' /etc/sudoers
-sudo /users/mdrodge/teamKMBR/master_oss.sh
+sudo /root/teamKMBR/master_oss.sh
