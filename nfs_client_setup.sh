6ab23337e8c3390b2844d2874d368f51c56a9d07
Ben Coomes <bcoomes@g.clemson.edu>
2017-04-06 23:30:12 -0400

    nfs client persistant mount

--- nfs_client_setup.sh
+++ nfs_client_setup.sh
@@ -2,8 +2,12 @@ yum install -y nfs-utils nfs-utils-lib
 
 mkdir /software
 
-#use server ip here
+
 mount nfsserv:/software /software
+
+#make mount persist on reboot
+echo "#SHARED SOFTWARE SETUP" >> /etc/fstab
+echo "nfsserv:/software /software nfs" >> /etc/fstab
 
 #to check it is mounted, not necessary:
 #df -h
