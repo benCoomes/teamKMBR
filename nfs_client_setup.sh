ff5f24b04881a3356cca8b2d0de3c12ebd379192
Benjamin Coomes <bcoomes@g.clemson.edu>
2017-04-06 23:33:07 -0400

    changed nfs client config to run remotely

--- nfs_client_setup.sh
+++ nfs_client_setup.sh
@@ -1,13 +1,13 @@
-yum install -y nfs-utils nfs-utils-lib
+ssh -t $1 'yum install -y nfs-utils nfs-utils-lib'
 
-mkdir /software
+ssh -t $1 'mkdir /software'
 
 
-mount nfsserv:/software /software
+ssh -t $1 'mount nfsserv:/software /software'
 
 #make mount persist on reboot
-echo "#SHARED SOFTWARE SETUP" >> /etc/fstab
-echo "nfsserv:/software /software nfs" >> /etc/fstab
+ssh -t $1 'echo "#SHARED SOFTWARE SETUP" >> /etc/fstab'
+ssh -t $1 'echo "nfsserv:/software /software nfs" >> /etc/fstab'
 
 #to check it is mounted, not necessary:
 #df -h
