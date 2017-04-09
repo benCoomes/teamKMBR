d202aaa55aae9e762fec9710e7299e9d3d2ca744
Ben Coomes <bcoomes@g.clemson.edu>
2017-04-09 13:52:01 -0400

    uncommented essential lines in nfs client setup

--- nfs_client_setup.sh
+++ nfs_client_setup.sh
@@ -11,8 +11,8 @@ ssh -t $1 'module use /software/Modules/modulefiles'
 
 echo "Done. Node configured for shared software use."
 #make mount persist on reboot
-#ssh -t $1 'sudo echo "#SHARED SOFTWARE SETUP" >> /etc/fstab'
-#ssh -t $1 'sudo echo "nfsserv:/software /software nfs" >> /etc/fstab'
+ssh -t $1 'sudo echo "#SHARED SOFTWARE SETUP" >> /etc/fstab'
+ssh -t $1 'sudo echo "nfsserv:/software /software nfs" >> /etc/fstab'
 
 #to check it is mounted, not necessary:
 #df -h
