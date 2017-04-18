82c2cd3695f8c7a7d982c901b09e72eef5d26413
Benjamin Coomes <bcoomes@g.clemson.edu>
2017-04-18 16:49:52 -0400

    deleted unneeded files

--- mpitest.py
+++
@@ -1,7 +0,0 @@
-from mpi4py import MPI
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-name = MPI.Get_processor_name()
-print("Hello from process %d on host %s out of %d processes" % (rank, name, size))
